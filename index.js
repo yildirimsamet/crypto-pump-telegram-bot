144f9c9617d25810e42408b1a09a34f5b0315c4c
Samet Yildirim <samety3503@gmail.com>
2023-03-04 17:03:50 +0300

    PumpConfig add

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
 import express from 'express';
 import { AlertStore } from './utils/AlertStorage.js';
-import { runSocket } from './utils/runSocket.js';
+import { pumpConfig, runSocket } from './utils/runSocket.js';
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
 
 runSocket();
 
-app.get('/last-alert', function (req, res) {
+app.get('/last-alert', (req, res) => {
     return res.send(AlertStore.getLastAlertTime());
 })
 
-app.get('/sended-coins', function (req, res) {
+app.get('/sended-coins', (req, res) => {
     return res.json({ data: AlertStore.getSendedCoinsInfo() });
 })
 
-app.listen(PORT, function () {
+app.post('/change-time-frame', (req, res) => {
+    const timeframe = req.body.timeframe;
+    if (timeframe) {
+        return res.send(pumpConfig.setTimeFrame(timeframe))
+    }
+    return res.send('timeframe parameter needed.')
+})
+
+app.post('/change-minimum-pump-rate', (req, res) => {
+    const minimumPumpRate = req.body.minimumPumpRate;
+    if (minimumPumpRate) {
+        return pumpConfig.setMinimumPumpRate(minimumPumpRate)
+    }
+    return res.send('minimumPumpRate parameter needed.')
+})
+
+app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
--- utils/runSocket.js
+++ utils/runSocket.js
@@ -3,19 +3,60 @@ import { sendErrorAlert, sendPumpAlert } from './sendTelegramAlert.js';
 import { AlertStore } from './AlertStorage.js'
 import { correlationList } from '../enums/generalEnums.js';
 
+class PumpConfig {
+    constructor () {
+        this.minimumPumpRate = '2';
+        this.timeFrame = '1m';
+        this.acceptedTimeFrames = ['1m', '5m', '15m', '1h'];
+    }
+
+    getMinimumPumpRate() {
+        return this.minimumPumpRate;
+    }
+
+    setMinimumPumpRate(minPumpRate) {
+        if (parseInt(minPumpRate)) {
+            this.minimumPumpRate = minPumpRate;
+
+            return 'Success';
+        }
+
+        return 'Please send a number';
+    }
+
+    getTimeFrame() {
+        return this.timeFrame;
+    }
+
+    setTimeFrame(timeFrame) {
+        if (this.acceptedTimeFrames.includes(timeFrame)) {
+            this.timeFrame = timeFrame;
+
+            return 'Success';
+        }
+
+        return 'Timeframe not allowed';
+    }
+}
+
+export const pumpConfig = new PumpConfig();
+
 export const runSocket = () => {
     connectSocket(async (data) => {
         try {
             if (data.success) {
                 const tickers = data.data;
                 const correlationFlatList = correlationList.flatMap((item) => item);
-                const minimumPumpRate = 2.5;
-                const timeFrame = '1m';
-        
+
+                console.log({
+                    timeframe : pumpConfig.timeFrame,
+                    minPump: pumpConfig.minimumPumpRate
+                });
+
                 const foundPumpedCoins = tickers.filter((tickerData) => {
                     const { symbol, price_change_pct } = tickerData;
         
-                    return correlationFlatList.includes(symbol) && price_change_pct[timeFrame] > minimumPumpRate
+                    return correlationFlatList.includes(symbol) && price_change_pct[pumpConfig.timeFrame] > pumpConfig.minimumPumpRate
                 })
         
                 for (let i = 0; i < foundPumpedCoins.length; i ++) {
@@ -26,13 +67,13 @@ export const runSocket = () => {
                             const alertPayload = {
                                 pumped: {
                                     symbol: symbol,
-                                    pumpRate: price_change_pct[timeFrame],
+                                    pumpRate: price_change_pct[pumpConfig.timeFrame],
                                 }
                             }
             
                             const correlatedCoin = correlationList.find(coinList => coinList.includes(symbol)).find(coinName => coinName != symbol);
                             const foundCoinPumpRateFromAllTickerData =
-                                ((tickers.find(ticker => ticker.symbol === correlatedCoin) || {}).price_change_pct || [])[timeFrame];
+                                ((tickers.find(ticker => ticker.symbol === correlatedCoin) || {}).price_change_pct || [])[pumpConfig.timeFrame];
         
                             if (correlatedCoin && foundCoinPumpRateFromAllTickerData) {
                                 alertPayload.correlated = {
